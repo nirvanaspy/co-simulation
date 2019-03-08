8c4510661b393aeec57b04c0b467d4fe09b1b944
nirvanaspy <nirvana_spy@outlook.com>
2019-03-08 15:55:41 +0800

    子任务管理页面和功能

---
+++ src/components/kanban/index.vue
@@ -0,0 +1,442 @@
+<template>
+  <div class="board-column">
+    <div class="board-column-header">
+      {{ headerText }}
+    </div>
+    <draggable
+      :list="list"
+      :options="options"
+      class="board-column-content">
+      <div v-for="element in list" :key="element.id" class="board-item" @click="handleEditTask(element)">
+        <span class="tag-box"></span>
+        <span v-if="element.designLinkEntity">{{element.designLinkEntity.description}}</span>
+        <span v-else>{{element.name}}</span>
+        <!--<span>{{ element.description }}</span>-->
+        <span style="float: right" @click="handleSelectTask(element)">
+          <svg-icon icon-class="enter"></svg-icon>
+        </span>
+        <span style="float: right;font-size: 12px;margin-right: 4px;" @click.stop="handleDeleteTask(element)">
+          <svg-icon icon-class="delete"></svg-icon>
+        </span>
+        <div class="tag-box-mine" v-if="element.finishTime">
+          <span class="status-box">
+            {{copmutedDate(element.finishTime)}}
+          </span>
+          <span class="link-type taskPic" v-if="element.userEntity">
+            <svg-icon icon-class="people"></svg-icon>{{element.userEntity.username}}
+          </span>
+        </div>
+      </div>
+      <div class="add-item" v-if="taskType == 'undo'" @click="handleAddTask">
+        <span>+</span>
+      </div>
+    </draggable>
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="newTaskDialog" append-to-body width="60%" class="limit-width-dialog">
+      <div class="img-container"></div>
+      <div class="text-container">为当前项目创建一个子任务</div>
+      <el-form label-position="left" label-width="120px">
+        <el-form-item label="任务名" prop="name">
+          <label slot="label">
+            <svg-icon icon-class="documentation"></svg-icon>
+            任务名
+          </label>
+          <el-input v-model="taskInfo.name"></el-input>
+        </el-form-item>
+        <!--<el-form-item :label="$t('table.projectDesc')" prop="description">
+          <label slot="label">
+            <svg-icon icon-class="components1"></svg-icon>
+            描述
+          </label>
+          <el-input v-model="taskInfo.description"></el-input>
+        </el-form-item>-->
+        <el-form-item :label="$t('table.projectDesc')" prop="description">
+          <label slot="label">
+            <svg-icon icon-class="people"></svg-icon>
+            执行者
+          </label>
+          <el-select v-model="taskInfo.userEntity" value-key="id" @focus="getAbleUser" placeholder="请选择" style="width: 100%">
+            <el-option
+              v-for="item in taskPicOptions"
+              :key="item.id"
+              :label="item.username"
+              :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item :label="$t('table.projectDesc')" prop="description">
+          <label slot="label">
+            <svg-icon icon-class="process"></svg-icon>
+            设计环节
+          </label>
+          <el-select v-model="taskInfo.designLinkEntity" value-key="description" @focus="getAbleLinks" placeholder="请选择" style="width: 100%">
+            <el-option
+              v-for="item in desiginLinkOptions"
+              :key="item.id"
+              :label="item.description"
+              :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <label slot="label">
+            <svg-icon icon-class="calendar"></svg-icon>
+            截止日期
+          </label>
+          <el-date-picker
+            style="width:100%;"
+            v-model="taskInfo.finishTime"
+            type="date"
+            placeholder="设置截止日期"
+            format="yyyy 年 MM 月 dd 日"
+            value-format="timestamp">
+          </el-date-picker>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button v-if="dialogStatus === 'create'" type="primary" @click="saveTask">确定</el-button>
+        <el-button v-if="dialogStatus === 'update'" type="primary" @click="editTask">确定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+  /*eslint-disable*/
+  import { allUser } from '@/api/getUsers'
+  import { setProLinks, updateProDesignLink, deleteSubtask } from '@/api/pro-design-link'
+  import { getLinks } from '@/api/designLinks'
+  import draggable from 'vuedraggable'
+  export default {
+    name: 'DragKanbanDemo',
+    components: {
+      draggable
+    },
+    props: {
+      taskType: {
+        type: String,
+        default: ''
+      },
+      headerText: {
+        type: String,
+        default: 'Header'
+      },
+      options: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      list: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        proId: '',
+        userId: '',
+        taskInfo: {
+          name: '',
+          info: '',
+          userId: '',
+          userEntity: {
+
+          },
+          designLinkEntity: {
+
+          },
+          finishTime: '',
+          linkId: ''
+        },
+        dialogStatus: '',
+        textMap: {
+          update: '修改任务',
+          create: '新建任务'
+        },
+        newTaskDialog: false,
+        taskPicOptions: [],
+        desiginLinkOptions: []
+      }
+    },
+    created() {
+      this.proId = this.$route.query.id
+      this.userId = this.getCookie('userId')
+      console.log(this.list)
+    },
+    methods: {
+      resetCreat() {
+        this.taskInfo = {
+          name: '',
+          info: '',
+          userId: '',
+          finishTime: '',
+          linkId: ''
+        }
+      },
+      handleAddTask() {
+        this.resetCreat()
+        this.dialogStatus = 'create'
+        this.newTaskDialog = true
+      },
+      getAbleUser() {
+        allUser().then((res) => {
+          if(res.data.code === 0) {
+            this.taskPicOptions = []
+            res.data.data.forEach((item) => {
+              this.taskPicOptions.push(item)
+            })
+            this.getMaLoading = false
+          } else {
+            this.getMaLoading = false
+          }
+        }).catch(() => {
+          this.getMaLoading = false
+        })
+      },
+      getAbleLinks() {
+        getLinks().then((res) => {
+          if(res.data.code === 0) {
+            this.desiginLinkOptions = res.data.data
+          }
+        })
+      },
+      saveTask() {
+        let qs = require('qs')
+        let data = {
+          finishTime: this.taskInfo.finishTime.toString(),
+          userId: this.taskInfo.userId,
+          designLinkEntityId: this.taskInfo.linkId
+        }
+        let dataPost = qs.stringify(data)
+        setProLinks(this.proId, dataPost).then((res) => {
+          if(res.data.code === 0) {
+            this.$notify({
+              title: '成功',
+              message: '任务新建成功！',
+              type: 'success',
+              duration: '2000'
+            })
+            this.newTaskDialog = false
+            // 成功后调用父组件方法 刷新list
+            this.$emit('onNewTask')
+          } else {
+            this.$notify({
+              title: '失败',
+              message: res.data.msg,
+              type: 'error',
+              duration: '2000'
+            })
+          }
+        })
+      },
+      handleEditTask(temp) {
+        console.log(temp)
+        this.dialogStatus = 'update'
+        this.taskInfo = Object.assign({}, temp)
+        if (temp.userEntity !== null) {
+          this.taskPicOptions = [temp.userEntity]
+        }
+        if (temp.designLinkEntity !== null) {
+          this.desiginLinkOptions = [temp.designLinkEntity]
+        }
+        this.newTaskDialog = true
+
+      },
+      editTask() {
+        let qs = require('qs')
+        let data = {
+          finishTime: this.taskInfo.finishTime.toString(),
+          // userId: this.taskInfo.userId,
+          userId: this.taskInfo.userEntity.id,
+          // designLinkEntityId: this.taskInfo.linkId
+          designLinkEntityId: this.taskInfo.designLinkEntity.id
+        }
+        let dataPost = qs.stringify(data)
+        updateProDesignLink(this.taskInfo.id, dataPost).then(res => {
+          if(res.data.code === 0) {
+            this.$notify({
+              title: '成功',
+              message: '任务修改成功！',
+              type: 'success',
+              duration: '2000'
+            })
+            this.newTaskDialog = false
+            // 成功后调用父组件方法 刷新list
+            this.$emit('onNewTask')
+          } else {
+            this.$notify({
+              title: '失败',
+              message: res.data.msg,
+              type: 'error',
+              duration: '2000'
+            })
+          }
+        })
+      },
+      handleDeleteTask(row) {
+        this.$confirm('确认删除吗？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          deleteSubtask(row.id).then((res) => {
+            if(res.data.code === 0) {
+              this.$notify({
+                title: '成功',
+                message: '任务删除成功！',
+                type: 'success',
+                duration: '2000'
+              })
+              // 成功后调用父组件方法 刷新list
+              this.$emit('onNewTask')
+            } else {
+              this.$notify({
+                title: '失败',
+                message: res.data.msg,
+                type: 'error',
+                duration: '2000'
+              })
+            }
+          })
+        }).catch(() => {
+
+        })
+      },
+      handleSelectTask(row) {
+        this.$router.push({
+          path: '/taskFiles',
+          name: 'taskFiles',
+          params: {
+            id: row.id
+          },
+          query: {
+            name: row.designLinkEntity.name
+          }
+        })
+        this.setCookie('taskSelectedName')
+      }
+    },
+    computed: {
+      copmutedDate() {
+        return function(time) {
+          let unixtimestamp = new Date(parseInt(time))
+          let year = unixtimestamp.getFullYear();
+          let month = "0" + (unixtimestamp.getMonth() + 1);
+          let date = "0" + unixtimestamp.getDate();
+          return year + "-" + month.substring(month.length-2, month.length)  + "-" + date.substring(date.length-2, date.length)
+        }
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .board-column {
+    min-width: 300px;
+    min-height: 100px;
+    height: auto;
+    overflow: hidden;
+    background: #f0f0f0;
+    border-radius: 3px;
+    .board-column-header {
+      height: 36px;
+      line-height: 36px;
+      overflow: hidden;
+      padding: 0 20px;
+      text-align: left;
+      background: #333;
+      color: #fff;
+      border-radius: 3px 3px 0 0;
+      font-size: 14px;
+    }
+    .board-column-content {
+      // background: #f2f8f9;
+      background: #f5f6f7;
+      height: auto;
+      overflow: hidden;
+      border: 10px solid transparent;
+      min-height: 60px;
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: column;
+      align-items: center;
+      .board-item {
+        position: relative;
+        cursor: pointer;
+        width: 100%;
+        height: 64px;
+        margin: 5px 0;
+        background-color: #fff;
+        text-align: left;
+        border-radius: 4px;
+        // line-height: 54px;
+        padding: 5px 14px;
+        box-sizing: border-box;
+        box-shadow: 0px 1px 3px 0 rgba(0, 0, 0, 0.2);
+        font-size: 13px;
+        color: #333;
+      }
+      .tag-box-mine {
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+      }
+      .status-box {
+        float: right;
+        display: inline-block;
+        width: 75px;
+        text-align: center;
+        background: rgb(255,79,62);
+        height: 20px;
+        line-height: 20px;
+        font-size: 12px;
+        color: #fff;
+        border-radius: 3px;
+        margin-top: 10px;
+        margin-right: -10px;
+      }
+      .taskPic {
+        // float: right;
+      }
+      .tag-box {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: inline-block;
+        height: 100%;
+        width: 2px;
+        background: #f9944a;
+      }
+      .board-item:hover {
+        .tag-box {
+          width: 4px;
+        }
+      }
+    }
+    .add-item {
+      width: 100%;
+      height: 30px;
+      line-height: 30px;
+      text-align: center;
+      background: #fff;
+      color: #ccc;
+      font-weight: 700;
+      font-size: 26px;
+      cursor: pointer;
+    }
+  }
+  .img-container {
+    width: 100%;
+    height: 140px;
+    margin: 0 auto;
+    background: url("./task-back.png") center center no-repeat;
+    background-size: 113px 90px;
+  }
+  .text-container {
+    text-align: center;
+    color: #777;
+    margin-bottom: 20px;
+    font-size: 14px;
+    font-weight: 500;
+  }
+</style>
---
+++ src/views/tasks/index.vue
@@ -0,0 +1,213 @@
+<template>
+  <div class="tasks-container">
+    <el-header style="background: linear-gradient(120deg, #00e4d0, #5983e8);line-height: 63px;height: 63px;">
+      <div class="searchContainer" style="display: inline-block;margin-bottom:16px;">
+        <span class="icon-search"><svg-icon icon-class="search"></svg-icon></span>
+        <el-input style="width: 160px;" class="filter-item" placeholder="任务名" v-model="searchQuery">
+        </el-input>
+        <div class="proTitle">
+          <svg-icon icon-class="co-simulation"></svg-icon>
+          CO-Simulation
+        </div>
+      </div>
+      <div class="right-menu">
+        <span calss="userName" style="position: relative;top: -12px;color: #fff;">{{userName}}</span>
+        <el-dropdown class="avatar-container right-menu-item" trigger="click">
+          <div class="avatar-wrapper">
+            <pan-thumb class="proImg" width="40px" height="40px" image="./2/png">
+            </pan-thumb>
+            <i class="el-icon-caret-bottom"></i>
+          </div>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <span v-if="role === 'admin'" style="display:block;">
+                <router-link to="/user_manage">用户管理</router-link>
+              </span>
+              <span v-else style="display:block;">修改密码</span>
+            </el-dropdown-item>
+            <el-dropdown-item divided>
+              <span @click="logout" style="display:block;">{{$t('navbar.logOut')}}</span>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+    <div style="float:right; text-align: right;margin: 20px 20px 0 0;">
+      <el-button type="primary" @click="handleSelectTemplate" size="mini">选择模版</el-button>
+    </div>
+    <div class="board">
+      <Kanban :key="1" :list="list" :options="options" :taskType="undo" class="kanban todo" header-text="待处理" @onNewTask="refreshList"/>
+      <Kanban :key="2" :list="list2" :options="options" class="kanban working" header-text="进行中"/>
+      <Kanban :key="3" :list="list3" :options="options" class="kanban done" header-text="已完成"/>
+    </div>
+    <el-dialog :visible.sync="templateDialog" width="80%" class="visio-dialog">
+      <visio :proId="proId" @refreshList="getLinkList"></visio>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  /*eslint-disable*/
+  import PanThumb from '@/components/PanThumb'
+  import Kanban from '@/components/kanban'
+  import visio from '@/views/visio/index'
+  import { getLinks } from '@/api/pro-design-link'
+  export default {
+    name: 'task_manage',
+    components: {
+      PanThumb,
+      Kanban,
+      visio
+    },
+    data() {
+      return {
+        undo: 'undo',
+        userId: '',
+        proId: '',
+        proName: '',
+        userName: '',
+        selectedId: '',
+        tableKey: 0,
+        list: [],
+        list1: [],
+        list2: [],
+        list3: [],
+        taskInfo: {
+          name: '',
+          info: ''
+        },
+        searchQuery: '',
+        formLabelWidth: '100px',
+        textMap: {
+          update: '编辑',
+          create: '新建'
+        },
+        token: '',
+        role: '',
+        options: {
+          group: 'mission'
+        },
+        temp: {
+          name: '',
+          pic: '',
+          finishTime: '',
+          designLink: null
+        },
+        templateDialog: false
+      }
+    },
+    created() {
+      this.proId = this.$route.query.id
+      this.proName = this.$route.query.name
+      this.getLinkList()
+    },
+    methods: {
+      getLinkList() {
+        getLinks(this.proId).then((res) => {
+          if(res.data.code === 0) {
+            this.list = res.data.data
+          }
+        })
+      },
+      logout() {
+        this.$store.dispatch('FedLogOut').then(() => {
+          location.reload()// In order to re-instantiate the vue-router object to avoid bugs
+        })
+      },
+      refreshList() {
+        this.getLinkList()
+      },
+      handleSelectTemplate() {
+        this.templateDialog = true
+      }
+    },
+    computed: {
+      /*list1: function() {
+        let self = this;
+        return self.list.filter(function (item) {
+          return item.state === 0;
+        })
+      },
+      list2: function() {
+        let self = this;
+        return self.list.filter(function (item) {
+          return item.state === 1;
+        })
+      },
+      list3: function() {
+        let self = this;
+        return self.list.filter(function (item) {
+          return item.state === 2;
+        })
+      }*/
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  $bg: #f1f3f5;
+  $dark_gray: #889aa4;
+  $light_gray: #eee;
+  .proTitle {
+    position: absolute;
+    width: 140px;
+    top: 0;
+    left: 50%;
+    margin-left: -70px;
+    background: linear-gradient(to right, #5786e8, #2961d2);
+    -webkit-background-clip: text;
+    color: transparent;
+  }
+  .icon-search {
+    position: absolute;
+    top: 1px;
+    left: 30px;
+    z-index: 99;
+  }
+  .right-menu {
+    float: right;
+    height: 100%;
+
+    &:focus {
+      outline: none;
+    }
+
+    .right-menu-item {
+      display: inline-block;
+      margin: 0 8px;
+    }
+
+    .avatar-container {
+      height: 50px;
+      margin-right: 30px;
+
+      .avatar-wrapper {
+        cursor: pointer;
+        margin-top: 5px;
+        position: relative;
+
+        .user-avatar {
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .el-icon-caret-bottom {
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
+    }
+  }
+  .board {
+    width: 1000px;
+    margin-left: 20px;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: row;
+    align-items: flex-start;
+    margin-top: 20px;
+  }
+</style>
